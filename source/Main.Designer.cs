99943f20dc4d9df5bc473c539d8cbc7b9f457ab5
S33TH3 <113664725+S33TH3@users.noreply.github.com>
2022-11-08 15:08:01 -0600

    initial commit

---
+++ source/Main.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Cat
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SpeechRecognitionBox = new System.Windows.Forms.TextBox();
+            this.TmrSpeaking = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Cat.Properties.Resources.cat_logo_transp;
+            this.pictureBox1.Location = new System.Drawing.Point(83, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(266, 274);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.pictureBox2.Location = new System.Drawing.Point(-1, -7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(444, 38);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(388, 2);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.label1.Font = new System.Drawing.Font("Nirmala UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(4, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CAT | A0.2";
+            // 
+            // SpeechRecognitionBox
+            // 
+            this.SpeechRecognitionBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.SpeechRecognitionBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SpeechRecognitionBox.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SpeechRecognitionBox.ForeColor = System.Drawing.Color.White;
+            this.SpeechRecognitionBox.Location = new System.Drawing.Point(12, 513);
+            this.SpeechRecognitionBox.Multiline = true;
+            this.SpeechRecognitionBox.Name = "SpeechRecognitionBox";
+            this.SpeechRecognitionBox.Size = new System.Drawing.Size(409, 69);
+            this.SpeechRecognitionBox.TabIndex = 4;
+            this.SpeechRecognitionBox.Text = "Recognized Speech";
+            // 
+            // TmrSpeaking
+            // 
+            this.TmrSpeaking.Enabled = true;
+            this.TmrSpeaking.Interval = 1000;
+            this.TmrSpeaking.Tick += new System.EventHandler(this.TmrSpeaking_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(408, 482);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(433, 594);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SpeechRecognitionBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SpeechRecognitionBox;
+        private System.Windows.Forms.Timer TmrSpeaking;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ source/Main.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cat.Modules;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Cat
+{
+    public partial class Main : Form
+    {
+        SpeechRecognitionEngine Recognition = new SpeechRecognitionEngine();
+        SpeechSynthesizer Catt = new SpeechSynthesizer();
+        SpeechRecognitionEngine IdleRecognition = new SpeechRecognitionEngine();
+        Choices choices = new Choices("What time is it", "How are you" , "Hey cat", "Stop listening");
+        int Timeout = 0;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Catt.SelectVoiceByHints(VoiceGender.Female);
+            Recognition.SetInputToDefaultAudioDevice();
+            Recognition.LoadGrammarAsync(new Grammar(new GrammarBuilder(choices)));
+            Recognition.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Recognition_SpeechRecognized);
+            //Recognition.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(Recognition_SpeechDetected);
+            Recognition.RecognizeAsync(RecognizeMode.Multiple);
+
+            IdleRecognition.SetInputToDefaultAudioDevice();
+            IdleRecognition.LoadGrammarAsync(new Grammar(new GrammarBuilder(choices)));
+            IdleRecognition.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(IdleRecognition_SpeechRecognized);
+        }
+
+        private void Recognition_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Timeout = 0;
+            SpeechRecognitionBox.Text = e.Result.Text;
+            switch (e.Result.Text)
+            {
+                case "What time is it":
+                    Catt.SpeakAsync(DateTime.Now.ToString("hh:mm tt"));
+                    break;
+
+                case "Stop listening":
+                    Recognition.RecognizeAsyncCancel();
+                    IdleRecognition.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void Recognition_SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void IdleRecognition_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "Hey cat":
+                    IdleRecognition.RecognizeAsyncCancel();
+                    Catt.SpeakAsync("How can I help you?");
+                    Recognition.RecognizeAsync(RecognizeMode.Multiple);
+                    Timeout = 0;
+                    break;
+            }
+        }
+
+        private void TmrSpeaking_Tick(object sender, EventArgs e)
+        {
+            Timeout += 1;
+            label2.Text = Timeout.ToString();
+            switch (Timeout)
+            {
+                case 10:
+                    Recognition.RecognizeAsyncCancel();
+                    break;
+
+                case 11:
+                    TmrSpeaking.Stop();
+                    IdleRecognition.RecognizeAsync(RecognizeMode.Multiple);
+                    Timeout = 0;
+                    break;
+            }
+        }
+    }
+}
