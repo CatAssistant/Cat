d1bd9270c0bbcadf14e70a8bb6ee59e11af4e4f7
S33TH3 <113664725+S33TH3@users.noreply.github.com>
2022-11-08 16:01:57 -0600

    organize & add git ignore

--- source/Main.cs
+++ source/Main.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Cat.Modules;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 
